3066f812f904cd804b3bce9fa69ec9434f11392d
ismailinayat <ismailinayat@gmail.com>
2021-11-24 20:48:12 +0500

    fixed url of frontend

--- server.js
+++ server.js
@@ -21,8 +21,8 @@ const server = app.listen(port, () => {
 
 const io = socket(server, {
   cors:{
-    origin:"http://localhost:3000"
-    //origin:"https://fridge-sigma.vercel.app"
+    //origin:"http://localhost:3000"
+    origin:"https://fridge-sigma.vercel.app"
   }
 })
 
